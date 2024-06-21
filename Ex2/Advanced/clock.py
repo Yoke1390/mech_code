06ecfe472be85f432cc68e8c1789a349a5dfe3d0
yosukemaeda <yoke1390@outlook.jp>
2024-06-21 15:39:26 +0900

    fix: 🐛 新しい分の計算方法の修正

--- Ex2/Advanced/clock.py
+++ Ex2/Advanced/clock.py
@@ -11,8 +11,6 @@ ser = serial.Serial('/dev/cu.usbserial-120', 9600)
 init_hour = 10
 init_minute = 10
 
-rotate_per_hour = 1  # 時計を1時間進めるのに必要なつまみの回転回数
-
 # =================================
 
 
@@ -66,7 +64,7 @@ class AnalogClock(tk.Canvas):
     def on_drag(self, event):
         x, y = event.x - self.center[0], event.y - self.center[1]
         angle = math.atan2(y, x) + math.pi / 2  # 90度のずれを修正
-        new_minute = (angle * 30 / math.pi) % 60
+        new_minute = int((angle * 30 / math.pi) % 60)
 
         # 現在時刻からのズレが大きい場合は無視
         new_x = self.center[0] + self.radius * 0.8 * math.sin(math.pi / 30 * new_minute)
@@ -76,19 +74,25 @@ class AnalogClock(tk.Canvas):
         if (new_x - old_x) ** 2 + (new_y - old_y) ** 2 > self.radius ** 2 / 25:
             return
 
+        # 分針が0時の位置をまたいだ場合の処理
 
+        minute_diff = new_minute - self.minute
+        # 反時計回りに回した場合
         if self.minute < 10 and new_minute > 50:
+            minute_diff -= 60
             self.hour = (self.hour - 1) % 24
+        # 時計回りに回した場合
         elif self.minute > 50 and new_minute < 10:
+            minute_diff += 60
             self.hour = (self.hour + 1) % 24
-        self.minute = round(new_minute)
-        self.draw_clock()
 
-    def send_serial(self):
-        diff_hour = self.hour - init_hour
-        diff_minute = self.minute - init_minute
-        target_angle = 2 * math.pi * rotate_per_hour * (diff_hour + diff_minute / 60)
-        ser.write(f"Move:{target_angle}")
+        self.minute = new_minute
+
+        # ステッピングモーターにステップ数を送信
+        step = int(minute_diff)
+        send_step(step)
+
+        self.draw_clock()
 
 
 def send_step(step):
