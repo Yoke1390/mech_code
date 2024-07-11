f8aaeb80a0206bc07f214b7a79e3746ca70b63ab
yosukemaeda <yoke1390@outlook.jp>
2024-07-11 13:56:33 +0900

    done problem8

--- Class/Soft2/2023-8/8Atest.cpp
+++ Class/Soft2/2023-8/8Atest.cpp
@@ -6,12 +6,19 @@ private:
   int balance;
 
 public:
-  Deposit(int init_balance) { balance = init_balance; }
+  int open_balance;
+
+  Deposit(int init_balance) {
+    balance = init_balance;
+    open_balance = balance * 2;
+  }
+
   int withdraw(int amount) {
     if (amount > balance) {
       cerr << "Insufficient funds" << endl;
     } else {
       balance = balance - amount;
+      open_balance -= amount * 2;
     }
     return balance;
   }
@@ -21,8 +28,9 @@ int main() {
   Deposit kei = Deposit(100);
   Deposit bill = Deposit(1000);
 
-  cout << typeid(kei).name() << endl;
-
+  // cout << "kei true balance: " << kei.balance << endl; // compile error
+  cout << "kei says: " << kei.open_balance << endl;
   cout << "kei  :" << kei.withdraw(25) << endl;
+  cout << "kei says: " << kei.open_balance << endl;
   cout << "bill :" << bill.withdraw(25) << endl;
 }
