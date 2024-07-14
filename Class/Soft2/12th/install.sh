1300c73c83bdce9a95a65c3e627a289ae5a57953
yosukemaeda <yoke1390@outlook.jp>
2024-07-15 00:14:54 +0900

    目標設定

---
+++ Class/Soft2/12th/install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install rabbitmq-server
+sudo apt install mosquitto mosquitto-clients
+sudo apt install python3-paho-mqtt
+sudo apt install python-zmq
+sudo apt install libzmqpp-dev
+sudo apt-get install libxmlrpc-core-c3-dev
+sudo apt install libxmlrpc-c++8-dev xmlrpc-api-utils
+sudo apt-get install omniidl omniidl-python libomniorb4-dev omniorb-nameserver
+sudo apt-get install omniidl-python
+sudo apt-get install python-omniorb
